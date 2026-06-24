5fffafa38ec6ed8225941466df027ae7a2db0dbb
Moniruzzaman <mk913724@gmail.com>
2026-06-25 01:09:21 +0600

    Enable headless mode for GitHub Actions

--- playwright.config.js
+++ playwright.config.js
@@ -2,29 +2,32 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
-  /* প্যারালাল রান বন্ধ করে সিরিয়ালি ১টি ওয়ার্কার দিয়ে রান করানো */
+
   fullyParallel: false,
   workers: 1,
   retries: 0,
-  /* পারফরম্যান্স গ্লিচ ইউজারের জন্য টাইম-আউট বাড়িয়ে ৪৫ সেকেন্ড করা হলো */
+
   timeout: 45000,
-  
+
   reporter: [
     ['html'],
     ['allure-playwright']
   ],
-  
+
   use: {
-    headless: false,
+    headless: true,
     testIdAttribute: 'data-test',
     screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
+    video: 'retain-on-failure'
   },
 
-  /* শুধুমাত্র ক্রোমিয়াম (Chrome) প্রজেক্টটি অন রাখা হলো বাকিগুলো বন্ধ */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome']
+      }
     }
-  ],
+  ]
 });
